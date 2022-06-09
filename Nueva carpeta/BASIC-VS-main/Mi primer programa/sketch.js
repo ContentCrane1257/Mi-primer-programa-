c544bdf5ea4ad0e6a418872f52b1879aa3149542
ContentCrane1257 <107224436+ContentCrane1257@users.noreply.github.com>
2022-06-09 17:57:25 -0500

    Add files via upload

---
+++ Nueva carpeta/BASIC-VS-main/Mi primer programa/sketch.js
@@ -0,0 +1,20 @@
+console.log("SALP")
+var trex ,trex_running;
+var Trex
+function preload(){
+  
+
+}
+
+function setup(){
+  createCanvas(600,200)
+  Trex = createSprite(300,50,25,25)
+  //create a trex sprite
+ 
+}
+
+function draw(){
+  background("black")
+  
+drawSprites();
+}
